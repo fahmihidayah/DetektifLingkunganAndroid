13e159258cf464486a92498fd0ed511dc8ee5357
fahmi <vambex.system@gmail.com>
2014-10-24 13:56:37 +0700

    set string value

--- gen/com/detektiflingkuganandroid/R.java
+++ gen/com/detektiflingkuganandroid/R.java
@@ -24,10 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int buttonDaftar=0x7f080004;
+        public static final int buttonLogin=0x7f080003;
+        public static final int editTextPassword=0x7f080002;
+        public static final int editTextUserName=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int login_activity=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
